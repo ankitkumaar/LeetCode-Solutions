23a173a22514c276722b6ca3c9dabce63c789b43
Ankit Kumaar <79037067+ankitkumaar@users.noreply.github.com>
2023-10-02 18:05:48 +0530

    Time: 31 ms (5.05%) | Memory: 44.7 MB (5.21%) - LeetSync

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -17,7 +17,10 @@ class Solution
                     {
                         sk_a.push('A');
                     }
-            
+
+            // If character is 'B' then
+            // Check character at index i is equal to character at index i+1
+            // Check character at index i+1 is equal to charactre at index i+2
             else if (colors.charAt(i) == 'B' && colors.charAt(i) == colors.charAt(i+1) && 
                     colors.charAt(i+1) == colors.charAt(i+2))
                     {
@@ -25,17 +28,25 @@ class Solution
                     }
         }
 
+        // Check if stack for 'A' or stack for 'B' is not empty
         while(!sk_a.isEmpty() || !sk_b.isEmpty())
         {
+            // If stack for 'A' is empty
+            // Return false
             if (sk_a.isEmpty())
                 return false;
             
+            // If stack for 'B' is empty
+            // Return true
             if (sk_b.isEmpty())
                 return true;
 
+            // pop one element for both stack 'A' and 'B'
             sk_a.pop();
             sk_b.pop();
         }
+        // Return false
+        // Its the case if both the stack is empty then 'B' wins
         return false;
     }
 }
